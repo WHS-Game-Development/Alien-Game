afcebe3b9fa2523f0c326c464393efcd1e2c496f
Om Dwivedi <26dwivediom@gmail.com>
2023-11-02 20:06:56 -0700

    Update
    Added gitignore, and changed the tilting of UFO and nerfed the crates.

--- Alien Game/Assets/Scripts/Sprites/Crate.cs
+++ Alien Game/Assets/Scripts/Sprites/Crate.cs
@@ -2,22 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class crate : MonoBehaviour {
+public class crate : MonoBehaviour
+{
+    public float moveSpeed = 2.0f;  // Adjust the speed as needed
+    private Rigidbody2D rb;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-	
-	void OnTriggerEnter2D(Collider2D col) {
-		if (col.CompareTag ("Projectile") ) {
-			transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
-			//velocity *= -1;
-		}
-	}
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // You can add any update logic you need here
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Projectile"))
+        {
+            // Invert the crate's horizontal velocity
+            rb.velocity = new Vector2(rb.velocity.x * -1, rb.velocity.y);
+        }
+    }
 }
